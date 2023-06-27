4bb6005b481fdef2b9340a23d7ef37e71a0249c4
Vitoria Vicentin <42698581+vitoriavicentin@users.noreply.github.com>
2023-06-27 17:53:07 +0000

    add docstring to def's

--- random_password.py
+++ random_password.py
@@ -1,15 +1,29 @@
 import random
 import string
 
+
 def get_random_string(lgth):
+    """
+    Generate a random password.
+    Args:
+        ch (str): A string for random letters.
+        lgth (int): Password length.
+    Returns:
+        str: A random password.
+    """
     ch = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(ch) for _ in range(lgth))
     return password
 
+
 def main():
+    """
+    Principal function to shown random password.
+    """
     password_lgth = 8
     random_password = get_random_string(password_lgth)
     print("A Random password is:", random_password)
+
 
 if __name__ == "__main__":
     main()
